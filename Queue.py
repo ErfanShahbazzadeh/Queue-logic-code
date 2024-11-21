8227b579cb0a5e9fd7357f57673cb344044eb58a
Erfan Shahbazzadeh <erfan.shahbazzadeh.83@gmail.com>
2024-11-21 20:29:27 +0330

    Base code

---
+++ Queue.py
@@ -0,0 +1,25 @@
+class queuecalss:
+    def __init__(self):
+        front = 0
+        rear = 0
+        Queue = list()
+
+    def Enqueue(self,object):
+        if IsEmpty() == True:
+            self.Queue[self.rear]  = object
+
+    def Dequeue(self):
+        if IsFull() == True:
+            return object
+    
+    def Peek(self):
+        return object
+
+    def ReverseQueue():
+        return queue
+
+    def IsEmpty(): 
+        return boolean
+
+    def IsFull(): 
+        return boolean
